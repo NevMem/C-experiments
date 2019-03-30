d664eb892e5a68067b85d0a742ddc3c0bfb6321d
NevMem <memlolkek@gmail.com>
2019-03-30 19:21:43 +0300

    Just for debug

--- main.c
+++ main.c
@@ -20,7 +20,14 @@ void register_parent_listener() {
 	signal(SIGRTMAX, parent_listener_sigrtmax);	
 }
 
-void proc_parent() {
+void proc_parent(int* first_child_pipe, int first_pid, int* second_child_pipe, int second_pid) {
+	register_parent_listener();
+
+	fprintf(first_child_pipe[1], "%d", second_pid);
+	close(first_child_pipe[1]);
+	fprintf(second_child_pipe[1], "%d", first_pid);
+	close(second_child_pipe[1]);
+	
 	while (1) {
 		if (parent_event_checker == -1) continue;
 		int cur = parent_event_checker;
@@ -37,33 +44,54 @@ void proc_parent() {
 	}
 }
 
+void proc_first(int* pipe) {
+	int brother;
+	fscanf(pipe[0], "%d", &brother);
+	fprintf(stderr, "Brother of first child has pid: %d", brother);
+}
+
+void proc_second(int* pipe) {
+	int brother;
+	fscanf(pipe[0], "%d", &brother);
+	fprintf(stderr, "Brother of second child has pid: %d", brother);
+}
+
+FILE* out;
+
 int main() {
 	printf("Start to perform our job\n");
+
+	out = fopen("output.txt", "w");
 
 	size_t n, m;
 	scanf("%lld %lld", &n, &m);
 
+	int first_pipe[2], second_pipe[2];
+	pipe(first_pipe);
+	pipe(second_pipe);
+
 	pid_t parent = getpid();
-	pid_t first_child = fork();
+	pid_t first_child = fork(), second_child;
 	if (first_child > 0) {
 		// Parent
 		fprintf(stderr, "Parent process is ready to create second child\n");
-		pid_t second_child = fork();
+		second_child = fork();
 		if (second_child < 0) {
 			fprintf(stderr, "Error occurred while forking second child\n");
 			exit(1);
 		} else if (second_child > 0) {
 			// Parent
-			fprintf(stderr, "Both children was successfully created (pids: %d %d)", first_child, second_child);
-			register_parent_listener();
-			proc_parent();
+			fprintf(stderr, "Children were successfully created (pids: %d %d)", first_child, second_child);
+			proc_parent(first_pipe, first_child, second_pipe, second_child);
 		} else {
 			// Second child
 			fprintf(stderr, "Second child is ready to perform his job\n");
+			proc_second(second_pipe);
 		}
 	} else if (first_child == 0) {
 		// First child
 		fprintf(stderr, "First child is ready to perform his job\n");
+		proc_first(first_pipe);
 	} else {
 		fprintf(stderr, "Error coccured while forking first child\n");
 	}
